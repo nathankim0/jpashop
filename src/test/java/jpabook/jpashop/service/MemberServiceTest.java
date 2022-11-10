53b9897e0d47ba88a86e87bc189b1c965ff32d0b
nathankim0 <jinyeob07@gmail.com>
2022-11-10 15:56:31 +0900

    feat: 중복회원예외 테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -36,9 +36,21 @@ public class MemberServiceTest {
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+        } catch (IllegalStateException e) {
+            return;
+        }
 
         //then
+        fail("예외가 발생해야 한다.");
     }
 }
