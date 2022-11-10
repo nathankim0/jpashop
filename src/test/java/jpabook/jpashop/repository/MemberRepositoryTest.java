1cbd21380d6a697401e16a3eae6c314341cde608
nathankim0 <jinyeob07@gmail.com>
2022-11-10 15:52:13 +0900

    bug: build error

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -20,15 +20,15 @@ public class MemberRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member();
-        member.setUsername("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
+        Member findMember = memberRepository.findOne(savedId);
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
         Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
 
     }
