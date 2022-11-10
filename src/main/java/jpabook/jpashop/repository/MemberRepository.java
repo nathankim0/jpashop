6aaa3474541584f4bfb441d151a2b268d11557a2
nathankim0 <jinyeob07@gmail.com>
2022-11-10 15:20:37 +0900

    refactor: member repository RequiredArgsConstructor

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,9 +10,10 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor // final 이 있는 필드만 가지고 생성자를 만들어준다.
 public class MemberRepository {
 
-    @PersistenceContext
+    // @PersistenceContext
     private EntityManager em;
 
     public Long save(Member member) {
