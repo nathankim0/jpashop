9db09c669d509207101817493cf1b58eedcbf254
nathankim0 <jinyeob07@gmail.com>
2022-11-10 15:57:11 +0900

    refactor: 어노테이션 활용

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -33,7 +33,7 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
@@ -44,11 +44,7 @@ public class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        try {
-            memberService.join(member2);
-        } catch (IllegalStateException e) {
-            return;
-        }
+        memberService.join(member2);
 
         //then
         fail("예외가 발생해야 한다.");
