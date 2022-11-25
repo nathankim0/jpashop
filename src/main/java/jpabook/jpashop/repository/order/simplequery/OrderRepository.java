06675ba103e34b5113d3ca3780c1a32f6a7a9107
nathankim0 <jinyeob07@gmail.com>
2022-11-25 12:21:27 +0900

    refactor: JPAQueryFactory

--- src/main/java/jpabook/jpashop/repository/order/simplequery/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderRepository.java
@@ -4,10 +4,7 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
-import jpabook.jpashop.domain.QMember;
-import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.repository.OrderSearch;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -18,10 +15,15 @@ import static jpabook.jpashop.domain.QMember.member;
 import static jpabook.jpashop.domain.QOrder.order;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -31,51 +33,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//        //language=JPAQL
-//        String jpql = "select o From Order o join o.member m";
-//
-//        boolean isFirstCondition = true;
-//
-//        //주문 상태 검색
-//        if (orderSearch.getOrderStatus() != null) {
-//            if (isFirstCondition) {
-//                jpql += " where";
-//                isFirstCondition = false;
-//            } else {
-//                jpql += " and";
-//            }
-//            jpql += " o.status = :status";
-//        }
-//
-//        //회원 이름 검색
-//        if (StringUtils.hasText(orderSearch.getMemberName())) {
-//            if (isFirstCondition) {
-//                jpql += " where";
-//                isFirstCondition = false;
-//            } else {
-//                jpql += " and";
-//            }
-//            jpql += " m.name like :name";
-//        }
-//
-//        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); //최대 1000건
-//
-//        if (orderSearch.getOrderStatus() != null) {
-//            query = query.setParameter("status", orderSearch.getOrderStatus());
-//        }
-//
-//        if (StringUtils.hasText(orderSearch.getMemberName())) {
-//            query = query.setParameter("name", orderSearch.getMemberName());
-//        }
-//
-//        return query.getResultList();
-//    }
-
     public List<Order> findAll(OrderSearch orderSearch) {
-        JPAQueryFactory query = new JPAQueryFactory(em);
-
         return query.select(order)
                 .from(order)
                 .join(order.member, member)
